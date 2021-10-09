2a6e7524e1f60005e8c2ca432a7e29862c42142b
Beder Bourahmah <bederbourahmah@gmail.com>
2021-10-09 09:37:42 -0700

    Add project files.

---
+++ LegendArenaBlazor/Client/LocalStorageKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegendArena.Blazor
+{
+  public static class LocalStorageKeys
+  {
+    public const string IsDarkThemePreferred = "IsDarkThemePreferred";
+  }
+}
