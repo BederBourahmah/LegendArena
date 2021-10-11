4c39741dfc8f27de26ba0dfb491de8effb7fd01c
Beder Bourahmah <bederbourahmah@gmail.com>
2021-10-10 18:04:59 -0700

    Added extension method to extract user id from http context.

---
+++ LegendArenaBlazor/Server/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LegendArena.WebApi.Extensions
+{
+  public static class HttpContextExtensions
+  {
+    public static string GetNameIdentifier(this HttpContext httpContext)
+      => httpContext.User.FindFirst(claim => claim.Type.Contains("nameidentifier"))?.Value;
+  }
+}
